a6e80ff6e52c4496d2c82eb9de011484a2646520
Vamsi Krishna Pamidi <krishpamidi9@gmail.com>
2024-12-02 15:27:36 +0530

    Add project files.

---
+++ FirstApp/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter;
+
+namespace FirstApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("9461c25d-ab5f-4c87-8772-4bdf6643d042",
+                  typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
